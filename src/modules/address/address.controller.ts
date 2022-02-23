433db0e07bfad3212f1c1b4768b2c5fb9d2deb16
Quan Thien Bao Tran <quan@glitch.finance>
2022-02-23 20:27:45 +0700

    update API validator and nominator

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -135,6 +135,29 @@ export class ValidatorController {
 
     return result;
   }
+
+  @Get(':hash/nominators')
+  async getNominators(@Param() params, @Query() query: any): Promise<any> {
+    const hash = params.hash;
+    const pageSize = Number(query.page_size) || 15;
+    const pageIndex = Number(query.page_index) || 1;
+
+    if (pageSize <= 0 || pageIndex <= 0) throw new BadRequestException();
+
+    let result: any;
+
+    try {
+      result = await this.addressService.getNominators(
+        hash,
+        pageSize,
+        pageIndex,
+      );
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+
+    return result;
+  }
 }
 
 @Controller('nominator')
@@ -152,6 +175,29 @@ export class NominatorController {
 
     try {
       result = await this.addressService.getNominatorList(pageSize, pageIndex);
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+
+    return result;
+  }
+
+  @Get(':hash/validators')
+  async getValidators(@Param() params, @Query() query: any): Promise<any> {
+    const hash = params.hash;
+    const pageSize = Number(query.page_size) || 15;
+    const pageIndex = Number(query.page_index) || 1;
+
+    if (pageSize <= 0 || pageIndex <= 0) throw new BadRequestException();
+
+    let result: any;
+
+    try {
+      result = await this.addressService.getValidators(
+        hash,
+        pageSize,
+        pageIndex,
+      );
     } catch (error) {
       throw new InternalServerErrorException();
     }
--- src/modules/address/address.providers.ts
+++ src/modules/address/address.providers.ts
@@ -1,10 +1,21 @@
 import { Connection } from 'typeorm';
-import { Address, BalanceHistory, Staking, Transaction } from '../../databases';
+import {
+  Address,
+  BalanceHistory,
+  NominatorValidator,
+  Transaction,
+} from '../../databases';
 
 export const AddressProviders = [
   {
     provide: 'ADDRESS_REPOSITORY',
     useFactory: (connection: Connection) => connection.getRepository(Address),
+    inject: ['DATABASE_CONNECTION'],
+  },
+  {
+    provide: 'NOMINATOR_VALIDATOR_REPOSITORY',
+    useFactory: (connection: Connection) =>
+      connection.getRepository(NominatorValidator),
     inject: ['DATABASE_CONNECTION'],
   },
   {
