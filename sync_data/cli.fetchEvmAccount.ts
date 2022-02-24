d407b97c2f4cd599d7babddf812ea80083b8bf26
Tom <to.luu2@sotatek.com>
2022-02-24 08:14:21 +0000

    sync evm

--- sync_data/cli.fetchEvmAccount.ts
+++ sync_data/cli.fetchEvmAccount.ts
@@ -34,6 +34,32 @@ const fetch = async (addressObj) => {
   }
 }
 
+
+const fetchEvm = async (addressObj) => {
+  let entityManager = getManager('postgres');
+  let api = Connection.httpApi
+  let now = new Date()
+  try {
+    let address = (await api.query.evmAccounts.accounts(addressObj.evmAddress))?.toString()
+    if (address) {
+      console.log(`${addressObj.evmAddress} linked ${address}`)
+      let addressEntity = await entityManager.findOne(Address, { where: { address: address } })
+      if (addressEntity) {
+          await Connection.connection.createQueryBuilder().delete().from(Address).where("id = :id", { id: Math.max(addressEntity.id, addressObj.id) }).execute()
+          await Connection.connection.createQueryBuilder().update(Address).set({ address: address, lastFetchEvm: now}).where("id = :id", { id:  Math.min(addressEntity.id, addressObj.id) }).execute()
+      } else {
+        await Connection.connection.createQueryBuilder().update(Address).set({ address: address, lastFetchEvm: now}).where("id = :id", { id: addressObj.id }).execute()
+      }
+      
+    } else {
+      await Connection.connection.createQueryBuilder().update(Address).set({ lastFetchEvm: now}).where("id = :id", { id: addressObj.id }).execute()
+      return true
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const fetchEvmAddress = async () => {
   // get transaction not fetched fee
   let entityManager = getManager('postgres');
@@ -41,6 +67,13 @@ const fetchEvmAddress = async () => {
   console.log("Find:", rows.length, "addresses")
   let funcs = rows.map(each => fetch(each))
   await Promise.all(funcs)
+
+  const evmRows = await entityManager.find(Address, { where: { address: IsNull() }, order: { lastFetchEvm: "ASC", }, take: THRESHOLD });
+  console.log("Find:", evmRows.length, " evm addresses")
+  funcs = evmRows.map(each => fetchEvm(each))
+  await Promise.all(funcs)
+
+
   await wait(3000)
   await fetchEvmAddress()
 }
